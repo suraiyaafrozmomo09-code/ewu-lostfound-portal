2ce1c1fa2a1ed076b4202eb15a862062c0a0298d
suraiyaafrozmomo09-code <suraiyaafrozmomo09@gmail.com>
2025-12-16 19:33:18 +0600

    Update ewu_lostfound.sql

--- ewu_lostfound.sql
+++ ewu_lostfound.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Dec 09, 2025 at 12:16 PM
+-- Host: 127.0.0.1
+-- Generation Time: Dec 16, 2025 at 02:31 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -63,6 +63,7 @@ CREATE TABLE `found_items` (
   `image_path` varchar(255) DEFAULT NULL,
   `status` enum('claimed','unclaimed') DEFAULT 'unclaimed',
   `claimed_by` int(11) DEFAULT NULL,
+  `claimed_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -70,8 +71,10 @@ CREATE TABLE `found_items` (
 -- Dumping data for table `found_items`
 --
 
-INSERT INTO `found_items` (`id`, `item_name`, `description`, `category_id`, `found_date`, `found_location`, `user_id`, `image_path`, `status`, `claimed_by`, `created_at`) VALUES
-(1, 'black wallet', 'black wallet with 1500 tk', 4, '2025-12-09', 'library', 10, NULL, 'unclaimed', NULL, '2025-12-09 09:47:16');
+INSERT INTO `found_items` (`id`, `item_name`, `description`, `category_id`, `found_date`, `found_location`, `user_id`, `image_path`, `status`, `claimed_by`, `claimed_at`, `created_at`) VALUES
+(1, 'black wallet', 'black wallet with 1500 tk', 4, '2025-12-09', 'library', 10, NULL, 'claimed', 13, '2025-12-16 13:21:27', '2025-12-09 09:47:16'),
+(2, 'sunglass', 'blue framed sunglass', 5, '2025-12-14', 'near old cafeteria', 13, NULL, 'unclaimed', NULL, NULL, '2025-12-15 16:15:55'),
+(3, 'id card', '2023-1-50-486', 6, '2025-12-16', 'library', 13, NULL, 'claimed', 10, NULL, '2025-12-16 13:25:02');
 
 -- --------------------------------------------------------
 
@@ -97,7 +100,8 @@ CREATE TABLE `lost_items` (
 --
 
 INSERT INTO `lost_items` (`id`, `item_name`, `description`, `category_id`, `lost_date`, `lost_location`, `user_id`, `image_path`, `status`, `created_at`) VALUES
-(2, 'iphone 14 plus', 'Yellow iphone cracked screen\r\nblue cover', 1, '2025-12-09', 'cafeteria', 10, NULL, 'lost', '2025-12-09 09:32:58');
+(2, 'iphone 14 plus', 'Yellow iphone cracked screen\r\nblue cover', 1, '2025-12-09', 'cafeteria', 10, NULL, 'lost', '2025-12-09 09:32:58'),
+(4, 'id card', 'id number 2023-1-60-098', 6, '2025-12-15', 'library', 13, NULL, 'lost', '2025-12-15 16:13:12');
 
 -- --------------------------------------------------------
 
@@ -139,7 +143,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `email`, `password`, `name`, `phone`, `role`, `created_at`) VALUES
 (10, 'student1@std.ewu.bd', '$2y$10$tQmBsY3q/wab8q334umPSuA4/I9O3OAWMOpMye5rjZk3x83hGNbiO', 'John Doe', '01710000001', 'student', '2025-12-06 22:51:12'),
 (11, 'student2@std.ewu.bd', '$2y$10$tQmBsY3q/wab8q334umPSuA4/I9O3OAWMOpMye5rjZk3x83hGNbiO', 'Jane Smith', '01710000002', 'student', '2025-12-06 22:51:12'),
-(12, 'admin@ewu.edu.bd', '$2y$10$tQmBsY3q/wab8q334umPSuA4/I9O3OAWMOpMye5rjZk3x83hGNbiO', 'System Admin', '01710000000', 'admin', '2025-12-06 22:51:12');
+(12, 'admin@ewu.edu.bd', '$2y$10$tQmBsY3q/wab8q334umPSuA4/I9O3OAWMOpMye5rjZk3x83hGNbiO', 'System Admin', '01710000000', 'admin', '2025-12-06 22:51:12'),
+(13, '2023-1-60-098@std.ewubd.edu', '$2y$10$27.ZOOC7grkLTJdC.ZBhMOmMquxICEcVuDtRCFxtqHa/G4mfHZ1lS', 'Suraiya Afroz', '01764189962', 'student', '2025-12-15 16:10:30');
 
 --
 -- Indexes for dumped tables
@@ -195,13 +200,13 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `found_items`
 --
 ALTER TABLE `found_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `lost_items`
 --
 ALTER TABLE `lost_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `messages`
@@ -213,7 +218,7 @@ ALTER TABLE `messages`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
